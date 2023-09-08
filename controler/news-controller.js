bb17d6a8914d3a7f20a10ed26d3ed9c5e1b3d3f7
Sunny12110867 <sunnybhardwaj456789@gmail.com>
2023-09-08 22:35:51 +0530

    removed database involvement

--- controler/news-controller.js
+++ controler/news-controller.js
@@ -1,10 +1,12 @@
 
 import News from '../model/news-schema.js'
 
+import {data} from "../constant/data.js"
+
 export const getNews = async (request,response)=>{
 
     try{
-       let data= await News.find({});
+       //let data= await News.find({});
        response.status(200).json(data);
     }catch(error){
         response.status(500).json({message: data.message});
